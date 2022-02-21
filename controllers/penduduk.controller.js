1c74de32a6cd8e656cff84413f0e826c899d5aae
FadillaRatnaDwita29RPL <fadilladwita20@gmail.com>
2022-02-21 11:48:21 +0700

    master

--- controllers/penduduk.controller.js
+++ controllers/penduduk.controller.js
@@ -2,6 +2,16 @@
 
 const db = require('../db')
 
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const secret = '#$@^%$^%*&%$$@&'
+
+function hashPassword(password){
+    const salt = bcrypt.genSaltSync(10)
+    return bcrypt.hashSync(password, salt)
+}
+
 module.exports = {
     read: (req, res) =>{
         const sql = "select * from data_penduduk"
@@ -15,15 +25,44 @@ module.exports = {
     },
 
     add: (req, res) =>{
-        const nama = req.body.nama
-        const alamat = req.body.alamat
-        const sql =  `insert into data_penduduk (nama, alamat) values ('${nama}','${alamat}')`
-        db.query(sql, (err, result) =>{
-            if(err) throw err
-            res.json({
-                message: "Berhasil menambahkan data"
+
+        const {
+            nama_lengkap,
+            alamat,
+            email,
+            password,
+
+        } = req.body
+        if (!nama_lengkap, !email || !password) {
+            res.status(402).json({
+                message: "nama lengkap, email dan password harus diisi!"
+            })
+        }
+        return db.query('insert into data_penduduk set ?', {
+            nama_lengkap,
+            alamat,
+            email,
+            password: hashPassword(password)
+        }, (err, result) => {
+            if (err) {
+                return res.status(500).json({
+                    err
+                })
+            }
+            return res.json({
+                message: 'registrasi berhasil',
+                data: result
             })
         })
+        // const nama = req.body.nama
+        // const alamat = req.body.alamat
+        // const sql =  `insert into data_penduduk (nama, alamat) values ('${nama}','${alamat}')`
+        // db.query(sql, (err, result) =>{
+        //     if(err) throw err
+        //     res.json({
+        //         message: "Berhasil menambahkan data"
+        //     })
+        // })
     },
 
     get: (req, res) =>{
@@ -42,7 +81,10 @@ module.exports = {
         const id = req.params.id
         const nama = req.body.nama
         const alamat = req.body.alamat
-        const sql = `update data_penduduk set nama='${nama}',alamat='${alamat}' where id='${id}'`
+        const password = req.body.password
+        const email = req.body.email
+        const pw = hashPassword(password)
+        const sql = `update data_penduduk set nama_lengkap='${nama}',alamat='${alamat}', password='${pw}', email='${email}' where id='${id}'`
         db.query(sql, (err, result) =>{
             if(err) throw err
             res.json({
@@ -51,7 +93,7 @@ module.exports = {
         })
     },
 
-    delete: (req, res) =>{
+    deletePenduduk: (req, res) =>{
         const id = req.params.id
         const sql = `delete from data_penduduk where id='${id}'`
         db.query(sql, (err, result) =>{
@@ -60,7 +102,30 @@ module.exports = {
                 message: "Berhasil menghapus data"
             })
         })
+    }, 
+
+    login: (req, res) => {
+        const {
+            email, 
+            password
+        } = req.body
+        
+        if( !email || !password) res.status(402).json({message: "email dan password harus diisi."})
+
+        return db.query('select * from data_penduduk where email = ?', email , (err, result)=>{
+            if(err) return res.status(500).json({err})
+
+            const user = result[0]
+            if(typeof user === 'undefined')return res.status(401).json({message: "user tidak ditemukan"})
+            if(!bcrypt.compareSync(password, user.password)) return res.status(401).json({message: "email atau password tidak sesuai"})
+
+            const token = jwt.sign({data: user}, secret)
+
+            return res.json({message: 'login berhasil. silahkan menggunakan token dibawah ini untuk mengakses endpoint private lain', token})
+        })
+
     }
+
 
 
 
--- routes/penduduk.route.js
+++ routes/penduduk.route.js
@@ -3,11 +3,24 @@
 const express = require('express')
 const pendudukController = require('../controllers/penduduk.controller')
 const router =  new express.Router();
+const {checkToken} = require("../authorization/token_validation")
+// const {
+//     read,
+//     add, 
+//     get, 
+//     edit,
+//     deletePenduduk 
+// } = require('../controllers/penduduk.controller')
 
-router.get("/", pendudukController.read)
+// router.route('/:id').get(get).delete(deletePenduduk).put(edit)
+// router.route('/').get(read).post(checkToken,add)
+
+router.get("/:id",checkToken, pendudukController.get)
+router.put("/:id",checkToken, pendudukController.edit)
+router.delete("/:id",checkToken, pendudukController.deletePenduduk)
+
+router.get("/",checkToken, pendudukController.read)
 router.post("/", pendudukController.add)
-router.get("/:id", pendudukController.get)
-router.put("/:id", pendudukController.edit)
-router.delete("/:id", pendudukController.delete)
+router.post("/login", pendudukController.login)
 
 module.exports = router
